3ec67b9ad3a82e504506a2df1e7e9bebcd9cf4c2
langdonglei <langdonglei@icloud.com>
2022-02-15 16:01:30 +0800

    rewrite poll

--- src/Throttle.php
+++ src/Throttle.php
@@ -5,9 +5,17 @@ namespace langdonglei;
 use Carbon\Carbon;
 use Exception;
 use Redis;
+use Throwable;
 
 class Throttle
 {
+    private Redis $redis;
+
+    public function __construct(Redis $redis)
+    {
+        $this->redis = $redis;
+    }
+
     public static function wait(Redis $redis, string $key, int $limit_minute, int $limit_hour = 0, int $limit_day = 0, int $reset_day_at = 0): int
     {
         $wait = function ($key, $limit, $expire) use ($redis) {
@@ -30,33 +38,32 @@ class Throttle
     }
 
     /**
-     * @throws Exception
+     * @throws Throwable
      */
-    public static function poll(Redis $redis, string $key, array $items, int $duration = 10, int $max_requests = 1): string
+    public function poll($items, $interval = 1, $interval_times = 1, $counter = 'counter'): string
     {
-        # 获取轮询的值匿名函数: $key的值 是一个计数器 对目标数组取余后 得到目标数组的索引值 从而达到轮询的目的
-        $poll = function ($items, $redis, $key) {
-            $v = $redis->incr($key);
-            if ($v == 1) {
-                # 计数器初始一个过期时间
-                $redis->expire($key, 86400 * 180);
-            }
-            return $items[$v % count($items)];
-        };
-        # 遍历目标数组
-        $count = count($items);
-        while ($count--) {
-            # 从当前轮询值开始
-            $item          = $poll($items, $redis, $key);
-            $request_times = $redis->get("$key:$item") ?? 0;
-            # 判断是否未满足限流条件 未限流 直接返回 被限流则继续下一个循环
-            if ($request_times < $max_requests) {
-                $request_times += 1;
-                $redis->setex("$key:$item", $duration, $request_times);
+        $ttl = $_ENV['THROTTLE_LOG_TTL'] ?? 86400 * 180;
+        $i   = count($items);
+        while ($i--) {
+            $kc   = "$counter:all";
+            $vc   = $this->inc($kc, $ttl);
+            $item = $items[$vc % count($items)];
+            $ki   = "$counter:$item";
+            $vi   = $this->inc($ki, $interval);
+            if ($vi <= $interval_times) {
+                $this->inc("$counter:valid", $ttl);
                 return $item;
             }
         }
-        # 如果一个符合条件的都没有找出来 那就没法往后继续 只能阻断程序了
         throw new Exception('busy');
+    }
+
+    private function inc($key, $ttl): int
+    {
+        $value = $this->redis->incr($key);
+        if ($this->redis->ttl($key) == -1) {
+            $this->redis->expire($key, $ttl);
+        }
+        return $value;
     }
 }
--- tests/ThrottleTest.php
+++ tests/ThrottleTest.php
@@ -3,30 +3,33 @@
 
 use langdonglei\Throttle;
 use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertTrue;
 
 class ThrottleTest extends TestCase
 {
     public function testPoll()
     {
-        $redis = new Redis();
-        $redis->connect('127.0.0.1');
-        $redis->auth('foobared');
-        var_dump(Throttle::poll($redis, 'test:2', [
-            'a',
-            'b',
-            'c',
-            'd',
-            'e',
-            'f',
-            'g'
-        ]));
+        try {
+            $redis = new Redis();
+            $redis->connect('127.0.0.1');
+            $item = (new Throttle($redis))->poll([
+                1,
+                2,
+                3
+            ], 10);
+            var_dump($item);
+
+        } catch (Throwable $e) {
+            var_dump($e->getMessage());
+            assertTrue(false);
+        }
+        assertTrue(true);
     }
 
     public function testWait()
     {
         $redis = new Redis();
         $redis->connect('127.0.0.1');
-        $redis->auth('foobared');
         var_dump(Throttle::wait($redis, 'test:1', 3));
     }
 }
